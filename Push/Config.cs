c65c23a1faf6dfba4fbefb45c535afc77bfb47a1
Alexander Betke <main@alexinabox.de>
2024-12-29 02:39:56 +0100

    initial push

---
+++ Push/Config.cs
@@ -0,0 +1,32 @@
+namespace Push
+{
+    using System.ComponentModel;
+
+    using Exiled.API.Features;
+    using Exiled.API.Interfaces;
+
+    /// <inheritdoc cref="IConfig"/>
+    public sealed class Config : IConfig
+    {
+        public Config()
+        {
+        }
+        /// <inheritdoc/>
+        public bool IsEnabled { get; set; } = true;
+
+        /// <inheritdoc />
+        public bool Debug { get; set; }
+
+        [Description("Hint displayed when the player was successfully pushed.")]
+        public Hint PlayerPushSuccessful { get; private set; } = new("<You <color=green>pushed</color> $player!", 1);
+
+        [Description("Hint displayed when the push is still on cooldown.")]
+        public Hint PlayerPushCooldown { get; private set; } = new("You cannot push yet! <color=yellow>Cooldown is active.</color>", 3);
+
+        [Description("Hint displayed when the push is from the wrong angle.")]
+        public Hint PlayerPushWrongAngle { get; private set; } = new("Push failed! <color=red>You need to align better.</color>", 3);
+
+        [Description("The unique id of the setting.")]
+        public int KeybindId { get; set; } = 202;
+    }
+}
